a7f589e73a66c9ff9a9cecab456f63938e2ddee0
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2021-12-01 17:18:43 +0900

    ログイン機能作成完了1回目

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'  
   get 'signup', to: 'users#new'
 
+
   resources :users, only: %i(new create)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
